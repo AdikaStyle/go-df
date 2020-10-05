aa59cd132eec316eec9c5259fa1dab9d8b0e8b37
matang <matang28@gmail.com>
2020-10-05 11:01:25 +0300

    Adding the concept of a Data Frame

---
+++ dataframe.go
@@ -0,0 +1,22 @@
+package df
+
+type Condition func(row Row) bool
+
+type JoinCondition func(left Row, right Row) bool
+
+type Aggregation func(column []CValue) CValue
+
+type DataFrame interface {
+	Select(columns ...string) DataFrame
+	Filter(cond Condition) DataFrame
+	Concat(with DataFrame) DataFrame
+	LeftJoin(with DataFrame, on JoinCondition) DataFrame
+	RightJoin(with DataFrame, on JoinCondition) DataFrame
+	InnerJoin(with DataFrame, on JoinCondition) DataFrame
+	OuterJoin(with DataFrame, on JoinCondition) DataFrame
+	Split(cond Condition) (onTrue DataFrame, onFalse DataFrame)
+	GroupBy(columns []string, aggregations map[string]Aggregation) DataFrame
+
+	GetRowCount() int
+	GetHeaders() []string
+}
